dcc05118b464992ed2777d3f58ee94083ff040dc
astyfx <astyfx@gmail.com>
2016-01-09 16:43:09 +0900

    Finish Markdown

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ db = SQLAlchemy(app)  # DB Connection Instance
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
+from flask.ext.misaka import Misaka
+Misaka(app)
+
 from models import *
 
 @login_manager.user_loader
--- static/bundle.js
+++ static/bundle.js
@@ -52,6 +52,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
+	__webpack_require__(3);
 
 
 /***/ },
@@ -59,6 +60,15 @@
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	(function () {
+	  $("#pagedown").pagedownBootstrap();
+	})();
+
 
 /***/ }
 /******/ ]);
